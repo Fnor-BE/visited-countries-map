e374cb089812f77679b32c84a6e3b26a0ae68598
Fnor <166872478+Fnor-BE@users.noreply.github.com>
2025-02-04 15:14:30 +0100

    Added visited and lived in countries

--- countries_visited_map.py
+++ countries_visited_map.py
@@ -5,9 +5,14 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_rows', 6)
 
 MAP_FILE = 'data/world.gpkg'
-DEFAULT_COLORS = plt.rcParams['axes.prop_cycle'].by_key()['color'][0]
+DEFAULT_COLOR = plt.rcParams['axes.prop_cycle'].by_key()['color'][0]
+BACKGROUND_COLOR = '#D3D3D3'
+EDGE_COLOR = 'white'
 
 class CountriesVisitedMap():
+    
+    fig = None
+    ax = None
     
     def __init__(self) -> None:
         gdf = gpd.read_file('data/world.gpkg')
@@ -27,17 +32,31 @@ class CountriesVisitedMap():
         self._gdf.loc[ self._gdf['name_long'] == country, 'status'] = status
         if year != 0:
             self._gdf.loc[ self._gdf['name_long'] == country, 'year_visited'] = year
+            
+    def add_country_visited(self, country:str, year:int = 0) -> None:
+        self.add_country(country, year, status='Visited')
     
-    def plot(self, color=DEFAULT_COLORS):
-        fig = plt.figure(figsize=(10,8))
-        ax = fig.add_axes([0, 0, 1, 1])
+    def add_country_lived_in(self, country:str, year:int = 0) -> None:
+        self.add_country(country, year, status='Lived in')
+    
+    def _new_plot(self, figsize=(10,8)):
+        self.fig = plt.figure(figsize=figsize)
+        self.ax = self.fig.add_axes([0, 0, 1, 1])
+        self.ax.axis('off')
+        self.ax.margins(0)
+    
+    def _plot_background(self, column:str, color=BACKGROUND_COLOR):
+        """Will draw all the countries where column is NaN"""
+        self._gdf[ self._gdf['status'].isna()].plot(ax=self.ax, color=color, edgecolor='white', linewidth=0.4)
+    
+    def plot(self, color=DEFAULT_COLOR, edgecolor=EDGE_COLOR):
+        self._new_plot()
+        self._gdf[ ~self._gdf['status'].isna() ].plot(ax=self.ax, color=color, edgecolor=edgecolor, linewidth=0.4)
+        self._plot_background('status')
 
-        self._gdf[ ~self._gdf['status'].isna() ].plot(ax=ax, color=color)
-        ax.axis('off')
-        ax.margins(0)
-
-        # gdf.explore(color=gdf['color'], missing_kwds={'color': 'lightgrey'})
-        self._gdf[ self._gdf['status'].isna()].plot(ax=ax, color='#D3D3D3', edgecolor='white', linewidth=0.4)
-        # plt.savefig('sample.png', bbox_inches="tight", pad_inches=0, transparent=True)
+    # gdf.explore(color=gdf['color'], missing_kwds={'color': 'lightgrey'})
+    # plt.savefig('sample.png', bbox_inches="tight", pad_inches=0, transparent=True)
+        
+    
 
 
